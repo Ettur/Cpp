135f15901acf45c6ee82bd648cb30fa567bfa506
N2942 <git config --global user.email "N2942@student.jamk.fi"git config --global user.email>
2020-03-02 09:33:21 +0200

    Added TicTacToe

--- Cpp/TicTacToe/Board.cpp
+++ Cpp/TicTacToe/Board.cpp
@@ -13,7 +13,7 @@ Board::Board() {
     State = { "1","2","3","4","5","6","7","8","9"};
 }
 
-static vector<string> Board::availableSquares(vector<string> board, string computer, string player) {
+vector<string> Board::availableSquares(vector<string> board, string computer, string player) {
     vector <string> availSqrs;
     for (size_t i = 0; i < board.size(); i++) {
         if ((board[i] != computer) && (board[i] != player)) {
--- Cpp/TicTacToe/Computer.h
+++ Cpp/TicTacToe/Computer.h
@@ -39,6 +39,73 @@ public:
             return draw;
         }
 
+        vector<Move> moves;
+
+        for (size_t i = 0; i < freeSquares.size(); i++)
+        {
+            Move move;
+            move.Index = stoi(freeSquares[i]);
+            board[freeSquares[i]] = player;
+
+            /* Tässä tapahtuu rekursio, jos on konen vuoro niin uusi MinMax kutsutaan pelaajan "vuorolla"
+             ja toisinpäin myös */
+
+            if (player == computer)
+            {
+
+                Move result = new Move();
+                result = (Move)MinMax(board, human);
+                move.score = result.score;
+            }
+            else
+            {
+                Move result = new Move();
+                result = (Move)MinMax(board, computer);
+                move.score = result.score;
+            }
+
+            // vapaudedaan käsitelyty ruutu ja lisätään siirto (move) moves listaan
+            board[freeSquares[i]] = move.index;
+            moves.Add(move);
+
+        }
+
+        /* kun kaikki mahdolliset siirrot yhdltä tasolta on kerätty, etsitään niistä paras mahdollinen
+        sille pelaajalle kene vuoro on - kone maximalisoi pelaaja minimalisoi*/
+        int bestMove = 0;
+
+        if (player == computer)
+        {
+            int bestScore = -10000;
+            for (int i = 0; i < moves.Count; i++)
+            {
+                if (moves[i].score > bestScore)
+                {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        else
+        {
+            int bestScore = 10000;
+            for (int i = 0; i < moves.Count; i++)
+            {
+                if (moves[i].score < bestScore)
+                {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+
+        //tason paras siirto löydetty, palautetaan se ylöspäin edelliselle pelaajalle
+        return moves[bestMove];
+
+    }
+
+
+
 }
 
 };
